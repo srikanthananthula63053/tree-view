66268d5c82ceb5c5d0b75d9805b62805147952b1
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-05 11:13:52 +0530

    adding react icons to the menu-items

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import MenuList from "./menu-list";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
 
 export default function MenuItem({ item }) {
     const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
@@ -17,12 +19,18 @@ export default function MenuItem({ item }) {
             <div style={{ display: 'flex', gap: '20px' }}>
                 <p>{item.label}</p>
                 {
-                    item && item.children && item.children.length  ?
-                        <span onClick={() => handleToggleChildren(item.label)}>+</span> : null
+                    item && item.children && item.children.length ?
+                        <span onClick={() => handleToggleChildren(item.label)}>
+                            {
+                                displayCurrentChildren[item.label] ?
+                                    <FaMinus color="#fff" size={25} /> : <FaPlus color="#fff" size={25} />
+                                    //'-' : '+'
+                            }
+                        </span> : null
                 }
             </div>
             {
-                item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? <MenuList list={item.children} /> : null
+                item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? <MenuList list={item.children} /> : null
             }
         </li>
     );

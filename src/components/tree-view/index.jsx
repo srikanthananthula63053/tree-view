fea64e2387bab1c4cd30dde5a2a0663bb5ba8a0d
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-05 11:33:57 +0530

    addin styles

--- src/components/tree-view/index.jsx
+++ src/components/tree-view/index.jsx
@@ -1,4 +1,6 @@
 import MenuList from "./menu-list";
+import "./styles.css";
+
 
 export default function TreeView({ menus = [] }) {
     return (
--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -16,7 +16,7 @@ export default function MenuItem({ item }) {
 
     return (
         <li>
-            <div style={{ display: 'flex', gap: '20px' }}>
+            <div className="menu-item" style={{ display: 'flex', gap: '20px' }}>
                 <p>{item.label}</p>
                 {
                     item && item.children && item.children.length ?

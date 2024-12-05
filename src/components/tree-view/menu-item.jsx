a5cebce1e60924b36ed716c168b40a38688fea2b
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-05 10:42:21 +0530

    adding onclick to the span

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import MenuList from "./menu-list";
 
 export default function MenuItem({ item }) {
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+    function handleToggleChildren(getCurrentlabel) {
+        setDisplayCurrentChildren({
+            ...displayCurrentChildren,
+            [getCurrentlabel]: !displayCurrentChildren[getCurrentlabel]
+        })
+    }
+    console.log(displayCurrentChildren);
 
     return (
         <li>
-           <p>{item.label}</p>
-           {
-            item && item.children && item.children.length > 0 ? <MenuList list={item.children}/>: null
-           }
+            <div style={{ display: 'flex', gap: '20px' }}>
+                <p>{item.label}</p>
+                {
+                    item && item.children && item.children.length  ?
+                        <span onClick={() => handleToggleChildren(item.label)}>+</span> : null
+                }
+            </div>
+            {
+                item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? <MenuList list={item.children} /> : null
+            }
         </li>
     );
 }
